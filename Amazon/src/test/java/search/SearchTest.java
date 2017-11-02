181af5ade9fea2a533bc9b6a131002325870cc7d
snjengathe11 <victory1213121>
2017-11-02 19:16:56 -0400

    Test Added for Login

--- Amazon/src/test/java/search/SearchTest.java
+++ Amazon/src/test/java/search/SearchTest.java
@@ -30,9 +30,9 @@ public class SearchTest extends CommonAPI{
         Thread.sleep(2000);
         clickByxpath("//*[@id=\"hlb-ptc-btn-native\"]");
         Thread.sleep(2000);
-//        typeByCssEnter("#ap_email", "pawan.jambhule@gmail.com");
-////        typeByCssEnter("#ap_password","p@ssw0rd");
-////        Thread.sleep(3500);
+        typeByCssEnter("#ap_email", "pawan.jambhule@gmail.com");
+        typeByCssEnter("#ap_password","p@ssw0rd");
+        Thread.sleep(3500);
 
     }
 
